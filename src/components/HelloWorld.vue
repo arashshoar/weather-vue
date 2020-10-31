f810ae8b138be7b0c69002b40b7b457eebb14ef3
Arash Soudishoar <b_arash_s@yahoo.com>
2020-10-31 07:26:23 -0700

    apckage-lock.json is added

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -19,7 +19,7 @@
       <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
       <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
     </ul>
-    <h3>Ecosystem Console: {{myConsole}}</h3>
+    <h3>Ecosystem</h3>
     <ul>
       <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
       <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
@@ -39,7 +39,7 @@ export default {
   },
   data: function () {
     return ({
-      styles: styles
+      styles,
     })
   },
   computed: {
