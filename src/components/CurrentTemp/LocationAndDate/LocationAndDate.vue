445475c993090b1cdf51ee75cc48a08a589e3ef2
Arash Soudishoar <b_arash_s@yahoo.com>
2020-11-05 05:12:00 -0800

    LocationAndDate styles is done

--- src/components/CurrentTemp/LocationAndDate/LocationAndDate.vue
+++ src/components/CurrentTemp/LocationAndDate/LocationAndDate.vue
@@ -1,20 +1,22 @@
 <template>
-  <div>
-    <div>In The Name of GOD</div>
-    <div>{{cityName}}</div>
-    <div>{{countryName}}</div>
-    <div>{{date}}, {{time}}</div>
+  <div :class="styles.locationAndDate">
+    <div :class="styles.inTheName">In The Name of GOD</div>
+    <div :class="styles.city">{{cityName}}</div>
+    <div :class="styles.country">{{countryName}}</div>
+    <div :class="styles.dateTime">{{date}}, {{time}}</div>
   </div>
 </template>
 
 <script>
+import styles from './LocationAndDate.module.scss'
+
 export default {
   name: 'LocationAndDate',
+  data() {
+    return {
+      styles
+    }
+  },
   props: ['cityName', 'countryName', 'date', 'time'],
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="scss" scoped>
-
-</style>
