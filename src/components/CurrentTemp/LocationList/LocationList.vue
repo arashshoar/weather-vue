2eb1043205aa0292a67032fa880f071266fea58a
Arash Soudishoar <b_arash_s@yahoo.com>
2020-11-04 21:27:45 -0800

    Bootstrap is added successfully

--- src/components/CurrentTemp/LocationList/LocationList.vue
+++ src/components/CurrentTemp/LocationList/LocationList.vue
@@ -1,6 +1,32 @@
 <template>
-  <div class="dropdown is-hoverable">
-    Location List Component
+  <div class="dropdown">
+    <button
+      class="btn btn-secondary dropdown-toggle"
+      type="button"
+      id="dropdownMenuButton"
+      data-toggle="dropdown"
+      aria-haspopup="true"
+      aria-expanded="false"
+    >
+      <!--   LocationIcon   -->
+      Dropdown button
+    </button>
+    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+
+      <div class="input-group md-3">
+        <input type="text" class="form-control"/>
+        <div class="input-group-append">
+          <button class="input-group-append" type="button">
+            Search
+          </button>
+        </div>
+      </div>
+
+
+      <a class="dropdown-item" href="#">Action</a>
+      <a class="dropdown-item" href="#">Another action</a>
+      <a class="dropdown-item" href="#">Something else here</a>
+    </div>
   </div>
 </template>
 
