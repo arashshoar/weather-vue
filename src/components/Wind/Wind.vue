783307a84e803e2cc0174d28e2be7316f4f8233d
Arash Soudishoar <b_arash_s@yahoo.com>
2020-11-25 11:46:36 -0800

    The Wind component and it's css are fine

--- src/components/Wind/Wind.vue
+++ src/components/Wind/Wind.vue
@@ -1,16 +1,37 @@
 <template>
-  <div>
-    <GadgetContainer :cardTitle="cardTitle">I am Wind component</GadgetContainer>
-  </div>
+  <GadgetContainer :cardTitle="cardTitle">
+    <div :class="`${styles.windContainer} clearfix`">
+      <div :class="`styles.items`">
+        <div :class="styles.bigGen">
+          WindGen
+        </div>
+        <div :class="styles.smallGen">
+          WindGen
+        </div>
+        <div :class="styles.wind">
+          <div>Wind</div>
+          <div>10 mph</div>
+        </div>
+        <div :class="styles.pressure">
+          <div>Barometer</div>
+          <div>20 hpa</div>
+        </div>
+      </div>
+      <div :class="styles.border"></div>
+    </div>
+  </GadgetContainer>
 </template>
 
 <script>
 import GadgetContainer from '../common/GadgetContainer/GadgetContainer'
 
+import styles from './Wind.module.scss'
+
 export default {
   name: 'Wind',
   data() {
     return ({
+      styles,
       cardTitle: "Wind & Pressure",
     })
   },
